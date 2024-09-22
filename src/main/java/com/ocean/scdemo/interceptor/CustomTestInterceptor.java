50ee70bbc6eb31d053f1280eac3b511ed24baea4
haeseoky <haeseoky@gmail.com>
2024-09-22 12:10:34 +0900

    feature: log, webmvcconfig, openapi, banner.txt

---
+++ src/main/java/com/ocean/scdemo/interceptor/CustomTestInterceptor.java
@@ -0,0 +1,31 @@
+package com.ocean.scdemo.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Component
+public class CustomTestInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("Pre Handle method is Calling");
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("Post Handle method is Calling");
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("Request and Response is completed");
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
---
+++ src/main/java/com/ocean/scdemo/util/LoggerUtil.java
@@ -0,0 +1,10 @@
+package com.ocean.scdemo.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerUtil {
+    public static <T> Logger getLogger(Class<T> clazz) {
+        return LoggerFactory.getLogger(clazz);
+    }
+}
